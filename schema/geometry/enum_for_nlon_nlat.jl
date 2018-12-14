27561d65684e2f865f4d4d5c973ba09a65093b34
Roel Stappers <roels@met.no>
2018-12-14 11:32:51 +0100

    add nlon_nlat.schema.json

---
+++ schema/geometry/enum_for_nlon_nlat.jl
@@ -0,0 +1,16 @@
+#!/usr/bin/env julia
+
+vals=Int64[];
+for i2 in 0:1:12
+  for i3 in 0:1:8 
+    for i5 in 0:1:5
+      val =  2^i2*3^i3*5^i5
+      if val < 2000
+        push!(vals, val)
+      end
+    end
+  end
+end
+
+sort!(vals)
+println(vals)
