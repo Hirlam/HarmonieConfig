536c15f629565c2f1aee32c397419fd320d2721a
Roel Stappers <Roel.stappers@gmail.com>
2018-12-15 10:59:00 +0100

    Update enum_for_nlon_nlat.jl

--- schema/geometry/enum_for_nlon_nlat.jl
+++ schema/geometry/enum_for_nlon_nlat.jl
@@ -1,11 +1,13 @@
 #!/usr/bin/env julia
 
+max=3000 
+
 vals=Int64[];
-for i2 in 0:1:12
-  for i3 in 0:1:8
-    for i5 in 0:1:5
+for i2 in 0:1:log(2,max )
+  for i3 in 0:1:log(3,max)
+    for i5 in 0:1:log(5,max)
       val =  2^i2*3^i3*5^i5
-      if val <= 3000
+      if val <= max
         push!(vals, val)
       end
     end
@@ -14,3 +16,9 @@ end
 
 sort!(vals)
 println(vals)
+
+
+# Or perhaps easier to read
+
+# using Primes
+# print([v for v in 1:max if all(factor(Vector, v) .<= 5)])
