210293ecd6e5a2a2a9aa7ea75d65535f04f455e6
Roel Stappers <roel.stappers@met.no>
2019-07-23 14:21:08 +0200

    Check diagnose in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,12 @@ import Harmonie, YAML
 
 using Test
 
-
+# Check valid config_exp.yml
 config = YAML.load(open("config/config_exp.yml"))
 @assert Harmonie.isvalid(config)
+@assert Harmonie.diagnose(config) == nothing   
 
+# Check non valid config_exp 
 config = YAML.load(open("config/config_exp_not_valid.yml"))
 @assert !Harmonie.isvalid(config)
 
