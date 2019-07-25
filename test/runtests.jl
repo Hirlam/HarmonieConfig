21fde1cd45e2eeca21acf37777995e57cad451f8
Roel Stappers <roel.stappers@met.no>
2019-07-25 13:19:35 +0200

    === nothing in runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 
-import Harmonie, YAML
+import Harmonie
+import YAML
 
 using Test
 
 # Check valid config_exp.yml
 config = YAML.load(open("config/config_exp.yml"))
 @assert Harmonie.isvalid(config)
-@assert Harmonie.diagnose(config) == nothing   
+@assert Harmonie.diagnose(config) === nothing   
 
 # Check non valid config_exp 
 config = YAML.load(open("config/config_exp_not_valid.yml"))
