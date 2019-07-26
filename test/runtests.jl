74b68feb4940f3f46224747164ccf3a1280bcccf
Roel Stappers <roel.stappers@met.no>
2019-07-26 17:26:28 +0200

    Moved config files

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,13 @@ import YAML
 using Test
 
 # Check valid config_exp.yml
-config = YAML.load(open("config/config_exp.yml"))
+config = YAML.load(open("config/good/config_exp.yml"))
 @assert Harmonie.isvalid(config)
 @assert Harmonie.diagnose(config) === nothing   
 
 # Check non valid config_exp 
-config = YAML.load(open("config/config_exp_not_valid.yml"))
+config = YAML.load(open("config/bad/config_exp_not_valid.yml"))
 @assert !Harmonie.isvalid(config)
 
-config = YAML.load(open("config/config_exp_Geometry_addProperties.yml"))
+config = YAML.load(open("config/bad/config_exp_Geometry_addProperties.yml"))
 @assert !Harmonie.isvalid(config)
