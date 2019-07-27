51acf2c6ebb5d5ee78f600b59ba45cfd47743b56
Roel Stappers <Roel.stappers@gmail.com>
2019-07-27 15:35:51 +0200

    Update unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,15 +5,16 @@ import YAML
 using Test
 
 # Check valid config_exp.yml
-@testset "good config_exp.yml" begin  
+@testset "Config_exp tests" begin  
   config = YAML.load(open("config/good/config_exp.yml"))
   @test_broken Harmonie.isvalid(config)
   @test_broken Harmonie.diagnose(config) === nothing
-end   
+ 
 
-# Check non valid config_exp 
-config = YAML.load(open("config/bad/config_exp_not_valid.yml"))
-@test !Harmonie.isvalid(config)
+  # Check non valid config_exp 
+  config = YAML.load(open("config/bad/config_exp_not_valid.yml"))
+  @test !Harmonie.isvalid(config)
 
-config = YAML.load(open("config/bad/config_exp_Geometry_addProperties.yml"))
-@test !Harmonie.isvalid(config)
+  config = YAML.load(open("config/bad/config_exp_Geometry_addProperties.yml"))
+  @test !Harmonie.isvalid(config)
+end 
