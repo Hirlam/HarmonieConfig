f641e7291a2515e83f35cc735ef1ff39ae75698d
Roel Stappers <Roel.stappers@gmail.com>
2019-07-27 15:18:48 +0200

    update directories

--- src/Harmonie.jl
+++ src/Harmonie.jl
@@ -6,8 +6,8 @@ import Base.isvalid
 export schema, isvalid, diagnose
 
 moduledir=@__DIR__ 
-schemafile="$moduledir/schema/harmonie.schema.json"
-schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory=moduledir) 
+schemafile="$moduledir/../docs/harmonie.schema.json"
+schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory="$moduledir/../docs") 
 
 
 isvalid(config::Dict)  = JSONSchema.isvalid(config,schema) 
