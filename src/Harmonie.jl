5d040e4f05a305ca552aa069dc20fcd99a3587c9
Roel Stappers <roel.stappers@met.no>
2019-07-30 15:17:18 +0200

    Update tests

--- src/Harmonie.jl
+++ src/Harmonie.jl
@@ -5,13 +5,14 @@ import Base.isvalid
 
 export schema, isvalid, diagnose
 
-moduledir=@__DIR__ 
-schemafile="$moduledir/../docs/harmonie.schema.json"
-schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory="$moduledir/../docs") 
+const moduledir=@__DIR__ 
+const schemafile="$moduledir/../docs/harmonie.schema.json"
+const schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory="$moduledir/../docs") 
 
 
 isvalid(config::Dict)  = JSONSchema.isvalid(config,schema) 
 diagnose(config::Dict) = JSONSchema.diagnose(config,schema)
+
 
 
 
