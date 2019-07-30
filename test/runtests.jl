84f03797e6edb958e80b57de5eab6390d8fec2fe
Roel Stappers <roel.stappers@met.no>
2019-07-30 13:37:38 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,12 +6,13 @@ using Test
 
 # Check valid config_exp.yml
 @testset "Config_exp tests" begin  
-  config = YAML.load(open("config/good/config_exp.yml"))
-  @test_broken Harmonie.isvalid(config)
-  @test_broken Harmonie.diagnose(config) === nothing
-  
+    
   config = TOML.parsefile("config/good/config_exp.toml")
   @test Harmonie.isvalid(config) 
+
+  config = TOML.parsefile("config/bad/unknown_domain.toml")
+  @test !Harmonie.isvalid(config) 
+
 
   # Check non valid config_exp 
   config = YAML.load(open("config/bad/config_exp_not_valid.yml"))
@@ -20,6 +21,14 @@ using Test
   config = YAML.load(open("config/bad/config_exp_Geometry_addProperties.yml"))
   @test !Harmonie.isvalid(config)
 
+  # Test that merged dictionaries pass the testbed
+  config     = TOML.parsefile("config/good/config_exp.toml")
+  arome3dvar = TOML.parsefile("config/good/arome_3dvar.toml")
+  newconfig  = merge(config,arome3dvar)
+  @test !Harmonie.isvalid(newconfig)
 
+  config = YAML.load(open("config/good/config_exp.yml"))
+  @test_broken Harmonie.isvalid(config)
+  @test_broken Harmonie.diagnose(config) === nothing
   
 end 
