ea3edffae30ef214a7d0199a3076d00641c093d9
Roel Stappers <roel.stappers@met.no>
2019-07-30 14:20:48 +0200

    Update runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,8 +10,9 @@ using Test
   config = TOML.parsefile("config/good/config_exp.toml")
   @test Harmonie.isvalid(config) 
 
-  config = TOML.parsefile("config/bad/unknown_domain.toml")
+  config = TOML.parsefile("config/bad/unknown_TOPO_SOURCE.toml")
   @test !Harmonie.isvalid(config) 
+  @test Harmonie.diagnose(config) == "in [Geometry.TOPO_SOURCE] : expected to be one of Any[\"gmted2010\", \"gtopo30\"]"
 
 
   # Check non valid config_exp 
@@ -24,8 +25,11 @@ using Test
   # Test that merged dictionaries pass the testbed
   config     = TOML.parsefile("config/good/config_exp.toml")
   arome3dvar = TOML.parsefile("config/good/arome_3dvar.toml")
-  newconfig  = merge(config,arome3dvar)
-  @test !Harmonie.isvalid(newconfig)
+
+  # See merge(combine, d::AbstractDict, others::AbstractDict...).
+  # Values with the same key will be combined using the combiner function. 
+  newconfig  = merge(merge, config, arome3dvar) 
+  @test Harmonie.isvalid(newconfig)
 
   config = YAML.load(open("config/good/config_exp.yml"))
   @test_broken Harmonie.isvalid(config)
