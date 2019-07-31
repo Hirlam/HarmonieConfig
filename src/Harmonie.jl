df6e4cc4b52d89caab1810bb3248ae068cf70ca4
Roel Stappers <roel.stappers@met.no>
2019-07-31 12:17:10 +0200

    Add documentation

--- src/Harmonie.jl
+++ src/Harmonie.jl
@@ -7,10 +7,32 @@ export schema, isvalid, diagnose
 
 const moduledir=@__DIR__ 
 const schemafile="$moduledir/../docs/harmonie.schema.json"
-const schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory="$moduledir/../docs") 
 
+"""
+    __init__()
 
+Initialize Harmonie.schema from Harmonie.schemafile. 
+
+"""
+function __init__()
+   global schema = JSONSchema.Schema(read(schemafile,String),parentFileDirectory="$moduledir/../docs") 
+   return nothing
+end 
+
+"""
+    isvalid(config)
+
+Check that `config` is valid against the Harmonie schema
+"""
 isvalid(config::Dict)  = JSONSchema.isvalid(config,schema) 
+
+"""
+    diagnose(config)
+
+Check that `config` is valid against the Harmonie schema. If
+valid return `nothing`, and if not, return a diagnostic String containing a
+selection of one or more likely causes of failure.
+"""
 diagnose(config::Dict) = JSONSchema.diagnose(config,schema)
 
 
