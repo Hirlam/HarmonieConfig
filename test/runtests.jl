38918b57e4e4e262c28caaff72d5e8e238d122e9
Roel Stappers <roel.stappers@met.no>
2019-07-31 12:16:54 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,11 @@ using Test
 # AROME 3DVAR using OULAN
 @testset "AROME_3DVAR" begin    
     config_exp  = TOML.parsefile("config/good/config_exp.toml")
-    arome_3dvar = TOML.parsefile("config/good/arome_3dvar.toml")
+    arome_3dvar = TOML.parsefile("config/good/harmonie_configurations/arome_3dvar.toml")
     config_exp_with_arome_3dvar = merge(merge, config_exp, arome_3dvar)
     @test Harmonie.isvalid(config_exp_with_arome_3dvar)
     @testset "OULAN" begin  
-       arome_3dvar_oulan = TOML.parsefile("config/good/arome_3dvar_oulan.toml")
+       arome_3dvar_oulan = TOML.parsefile("config/good/harmonie_configurations/testbed/arome_3dvar_oulan.toml")
        newconfig = merge(merge, config_exp_with_arome_3dvar, arome_3dvar_oulan)        
        @test_broken Harmonie.isvalid(newconfig)
     end
@@ -19,10 +19,10 @@ end
 
 @testset "AROME_4DVAR" begin
     config_exp  = TOML.parsefile("config/good/config_exp.toml")
-    arome_4dvar = TOML.parsefile("config/good/arome_4dvar.toml")
+    arome_4dvar = TOML.parsefile("config/good/harmonie_configurations/arome_4dvar.toml")
     newconfig = merge(merge,config_exp,arome_4dvar)
     @test Harmonie.isvalid(newconfig) 
-    testbed_arome_4dvar = TOML.parsefile("config/good/testbed/arome_4dvar.toml")
+    testbed_arome_4dvar = TOML.parsefile("config/good/harmonie_configurations/testbed/arome_4dvar.toml")
     newconfig = merge(merge,newconfig,testbed_arome_4dvar)
     @test Harmonie.isvalid(newconfig) 
 end 
@@ -46,15 +46,6 @@ end
 
   config = YAML.load(open("config/bad/config_exp_Geometry_addProperties.yml"))
   @test !Harmonie.isvalid(config)
-
-  # Test that merged dictionaries pass the testbed
-  config     = TOML.parsefile("config/good/config_exp.toml")
-  arome3dvar = TOML.parsefile("config/good/arome_3dvar.toml")
-
-  # See merge(combine, d::AbstractDict, others::AbstractDict...).
-  # Values with the same key will be combined using the combiner function. 
-  newconfig  = merge(merge, config, arome3dvar) 
-  @test Harmonie.isvalid(newconfig)
 
   config = YAML.load(open("config/good/config_exp.yml"))
   @test_broken Harmonie.isvalid(config)
