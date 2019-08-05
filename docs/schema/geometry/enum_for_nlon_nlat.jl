53e5afa87ccad3a3173bec5f4a6d89e53adc8e2f
Roel Stappers <roel.stappers@met.no>
2019-08-05 16:18:11 +0200

    Remove odd values from nlat nlon schema

--- docs/schema/geometry/enum_for_nlon_nlat.jl
+++ docs/schema/geometry/enum_for_nlon_nlat.jl
@@ -2,8 +2,9 @@
 
 max=3000 
 
+# i2 start from 1 because we even numbers
 vals=Int64[];
-for i2 in 0:1:log(2,max )
+for i2 in 1:1:log(2,max )
   for i3 in 0:1:log(3,max)
     for i5 in 0:1:log(5,max)
       val =  2^i2*3^i3*5^i5
